0157eb5636f98aa690b5f74ddc8d2a96757f7d3d
Sanjeev Kumar Dwivedi <sanjeevdwivedi7777@gmail.com>
2026-03-17 10:02:49 +0530

    Time: 10 ms (32.95%), Space: 47.6 MB (55.6%) - LeetHub

---
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        Arrays.sort(nums);
+        int i=0;
+        for(i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                break;
+            }
+        }
+        return nums[i];
+    }
+}
