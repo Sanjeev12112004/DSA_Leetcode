3d09f2be74d7dff016eeb6fdd94832d39967de84
Sanjeev Kumar Dwivedi <sanjeevdwivedi7777@gmail.com>
2026-03-24 03:36:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
@@ -1,11 +1,5 @@
 class Solution {
     public int totalMoney(int n) {
-        int s1=n/7;
-        int sum1=7*(s1*(8+(s1-1))/2);
-        System.out.println(sum1);
-        int s2=n%7;
-        int start=(s1+1);
-        int sum2=s2*(2*start+(s2-1))/2;
-        return sum1+sum2;
+        return 7*((n/7)*(8+((n/7)-1))/2)+(n%7)*(2*((n/7)+1)+((n%7)-1))/2;
     }
 }
