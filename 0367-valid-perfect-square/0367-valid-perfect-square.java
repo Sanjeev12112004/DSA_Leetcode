cbb6e580a935e0e6edfa3e36b228bd2d5c3c4797
Sanjeev Kumar Dwivedi <sanjeevdwivedi7777@gmail.com>
2026-03-28 18:08:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
         if(num==1) return true; 
-        for(long i=1;i<num;i++){
+        for(long i=1;i*i<=num;i++){
             if(i*i==num){
                 return true;
             }
