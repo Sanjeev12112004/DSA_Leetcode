695cac32650ecf122698322128b6a0587dc5742e
Sanjeev Kumar Dwivedi <sanjeevdwivedi7777@gmail.com>
2026-04-25 22:36:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3908-valid-digit-number/3908-valid-digit-number.java
+++ 3908-valid-digit-number/3908-valid-digit-number.java
@@ -1,21 +1,23 @@
 class Solution {
     public boolean validDigit(int n, int x) {
-        int rev=0;
-        int n2=n;
-        while(n2!=0){
-            int l=n2%10;
-            rev=rev*10+l;
-            n2=n2/10;
-        }
-        if(rev%10==x) return false;
+        // int rev=0;
+        // int n2=n;
+        // while(n2!=0){
+        //     int l=n2%10;
+        //     rev=rev*10+l;
+        //     n2=n2/10;
+        // }
+        // if(rev%10==x) return false;
+        int last=-1;
         boolean found=false;
         while(n!=0){
             if(n%10==x){
                 found=true;
-                break;
             }
+            last=n%10;
             n=n/10;
+
         }
-        return found;
+        return found && (last!=x);
     }
 }
