4b86d58ed6cbdc549da63822b18aeb577537c968
Sanjeev Kumar Dwivedi <sanjeevdwivedi7777@gmail.com>
2026-04-25 22:57:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3909-compare-sums-of-bitonic-parts/3909-compare-sums-of-bitonic-parts.java
+++ 3909-compare-sums-of-bitonic-parts/3909-compare-sums-of-bitonic-parts.java
@@ -1,25 +1,12 @@
 class Solution {
     public int compareBitonicSums(int[] nums) {
-        long sum1=0;
-        long sum2=0;
-        boolean flag=true;
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i]<nums[i+1]){
-                sum1=sum1+nums[i];
-                flag=true;
-            }
-            else{
-                sum2=sum2+nums[i];
-                if(flag){
-                    sum1=sum1+nums[i];
-                    flag=false;
-                }
-            }
+        long sum=0;
+        for(int i=0; i<nums.length-1;i++){
+            if(nums[i]>nums[i+1])sum-=nums[i+1];
+            else sum+=nums[i];
         }
-        sum2=sum2+nums[nums.length-1];
-        System.out.println(sum1+" "+sum2);
-        if(sum1>sum2) return 0;
-        else if(sum2>sum1) return 1;
-        return -1;
+        if(sum==0)return -1;
+        else if(sum>0)return 0;
+        else return 1;
     }
 }
