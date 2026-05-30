51bbc2655702e6b0c6f1b5c1d03fe1fe9fcf74fd
Sanjeev Kumar Dwivedi <sanjeevdwivedi7777@gmail.com>
2026-05-30 19:48:06 +0530

    Time: 1 ms (55.68%), Space: 45.1 MB (34.43%) - LeetHub

---
+++ 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countTestedDevices(int[] batteryPercentages) {
+        int count=0;
+        for(int i=0;i<batteryPercentages.length;i++){
+            int d=batteryPercentages[i];
+            if(d>0){
+                count++;
+                for(int j=i+1;j<batteryPercentages.length;j++){
+                    batteryPercentages[j]=batteryPercentages[j]-1;
+                }
+            }
+        }
+        return count;
+    }
+}
