440fc7e8b691d1c814c7d3c2b5953d0c3f889695
Sanjeev Kumar Dwivedi <sanjeevdwivedi7777@gmail.com>
2026-06-06 21:00:53 +0530

    Time: 2 ms (44.64%), Space: 48.9 MB (50.01%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,39 +1,29 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-      int temp[]= new int[nums1.length+nums2.length];
-      
-      int i=0;
-      int j=0;
-      int k=0;
-    while(i<nums1.length && j<nums2.length){
-           if(nums1[i]>nums2[j]){
-            temp[k]=nums2[j];
-            j++;
-           }
-           else{
+       int temp[]=new int[nums1.length+nums2.length];
+       int i=0;
+       int j=0;
+       int k=0;
+       while(i<nums1.length&& j<nums2.length){
+          if(nums1[i]<nums2[j]){
             temp[k]=nums1[i];
             i++;
-           }
-           k++;     
+          }
+          else{
+            temp[k]=nums2[j];
+            j++;
+          }
+          k++;
+       }
+        while(i<nums1.length){
+            temp[k++]=nums1[i++];
+        }
+        while(j<nums2.length){
+            temp[k++]=nums2[j++];
+        }
+        if(temp.length%2!=0) return (double)temp[temp.length/2];
+        else{
+            return (double)(temp[temp.length/2]+temp[temp.length/2 -1])/2;
+        } 
     }
-    while(i<nums1.length){
-        temp[k++]=nums1[i++];
-    }
-    while(j<nums2.length){
-        temp[k++]=nums2[j++];
-    }
-    int n=(temp.length)/2;
-
-    if(temp.length%2!=0){
-        return (double)temp[n];
-    }
-    else{
-        int s=0;
-        int end= temp.length-1;
-        int mid=(s+end)/2;
-
-        return  (double)(temp[mid]+temp[mid+1])/2;
-    }
-    
-  }
 }
